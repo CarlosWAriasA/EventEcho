ab55bcd295d61d264660caf79994c3af29d571eb
JosueNavarrop <josuenavarrop2003janp@gmail.com>
2024-02-10 18:16:41 -0400

    Se a cambiado el klk por un hola

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+// Creamos una instancia de express
+const app = express();
+
+// Definimos una ruta
+app.get('/', (req, res) => {
+  res.send('HOLA!');
+});
+
+// Iniciamos el servidor en el puerto 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor activo en http://localhost:${PORT}`);
+});
