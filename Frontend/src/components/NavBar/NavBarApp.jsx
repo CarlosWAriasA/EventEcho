6dd08b6c0e058e29d3492bf84f3ee58c2eddea9e
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2024-03-09 10:22:25 -0400

    Agregando pantalla de account

--- Frontend/src/components/NavBar/NavBarApp.jsx
+++ Frontend/src/components/NavBar/NavBarApp.jsx
@@ -1,4 +1,11 @@
-import { CircleUserRound, LogOut, UserRound, ArrowBigLeft } from "lucide-react";
+import {
+  CircleUserRound,
+  LogOut,
+  UserRound,
+  ArrowBigLeft,
+  Bell,
+  CalendarCheck,
+} from "lucide-react";
 import { NavLink, useLocation } from "react-router-dom";
 import { useContext, useState, useEffect, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext";
@@ -49,10 +56,29 @@ const NavbarApp = () => {
             <div className="text-black ml-10 text-lg mt-2">EventEcho</div>
           </NavLink>
         </li>
-        {isEventDetail ? (
-          <li>
+        <li className="flex gap-6 content-center pt-3">
+          <NavLink to={"/home"}>
+            <div className="">
+              <CalendarCheck
+                className="hover:cursor-pointer select-none"
+                color="black"
+                size={25}
+              />
+            </div>
+          </NavLink>
+          <NavLink to={"/home"}>
+            <div className="">
+              <Bell
+                className="hover:cursor-pointer select-none"
+                color="black"
+                size={25}
+                fill="black"
+              />
+            </div>
+          </NavLink>
+          {isEventDetail ? (
             <NavLink to={"/"}>
-              <div className="mr-10 mt-3">
+              <div className="mr-10">
                 <ArrowBigLeft
                   className="hover:cursor-pointer select-none"
                   color="black"
@@ -65,39 +91,41 @@ const NavbarApp = () => {
                 />
               </div>
             </NavLink>
-          </li>
-        ) : (
-          <li ref={optionsRef}>
-            <div className="mr-10 mt-2">
-              <CircleUserRound
-                className="hover:cursor-pointer select-none"
-                color="black"
-                onClick={handleIconClick}
-                size={25}
-              />
-            </div>
-            {showOptions && (
-              <div className="absolute right-12 w-44 bg-gray-800 rounded-md ">
-                <ul>
-                  <li
-                    onClick={handleProfileClick}
-                    className="flex items-center gap-2 p-2 cursor-pointer hover:bg-gray-400 hover:rounded-t-md"
-                  >
-                    <UserRound size={18} />
-                    Profile
-                  </li>
-                  <li
-                    onClick={handleLogoutClick}
-                    className="flex items-center gap-2 p-2 cursor-pointer hover:bg-gray-400 hover:rounded-b-md"
-                  >
-                    <LogOut size={18} color="red" />
-                    Log Out
-                  </li>
-                </ul>
+          ) : (
+            <div>
+              <div className="mr-10" ref={optionsRef}>
+                <CircleUserRound
+                  className="hover:cursor-pointer select-none"
+                  color="black"
+                  onClick={handleIconClick}
+                  size={25}
+                />
               </div>
-            )}
-          </li>
-        )}
+              {showOptions && (
+                <div className="absolute right-12 w-44 bg-gray-800 rounded-md ">
+                  <ul>
+                    <NavLink to={"account-settings"}>
+                      <li
+                        onClick={handleProfileClick}
+                        className="flex items-center gap-2 p-2 cursor-pointer hover:bg-gray-400 hover:rounded-t-md"
+                      >
+                        <UserRound size={18} />
+                        Profile
+                      </li>
+                    </NavLink>
+                    <li
+                      onClick={handleLogoutClick}
+                      className="flex items-center gap-2 p-2 cursor-pointer hover:bg-gray-400 hover:rounded-b-md"
+                    >
+                      <LogOut size={18} color="red" />
+                      Log Out
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
+        </li>
       </ul>
     </nav>
   );
---
+++ Frontend/src/views/Account/AccountSettings.jsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+
+function AccountSettings() {
+  const [user, setUser] = useState({
+    name: "",
+    password: "",
+    email: "",
+    age: "",
+    description: "",
+  });
+
+  return (
+    <main className="bg-white h-full text-black pt-10 pl-16">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-lg " style={{ fontSize: "50px" }}>
+          Account Settings
+        </h2>
+        <div className="w-16 h-3 bg-yellow-400 rounded-sm"></div>
+      </div>
+      <div className="flex">
+        <div className="mt-14" style={{ width: "50%" }}>
+          <div className="flex gap-20">
+            <div className="flex flex-col w-64">
+              <label htmlFor="name">Nombre</label>
+              <input
+                id="name"
+                className="border p-2 rounded-md border-gray-500"
+                placeholder="Nombre"
+                type="text"
+                value={user.name}
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, name: e.target.value }))
+                }
+              />
+            </div>
+            <div className="flex flex-col w-80">
+              <label>Email</label>
+              <input
+                className="border p-2 rounded-md border-gray-500"
+                placeholder="Email"
+                type="email"
+                value={user.email}
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, email: e.target.value }))
+                }
+              />
+            </div>
+          </div>
+          <div className="flex gap-20 mt-5">
+            <div className="flex flex-col w-64">
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                className="border p-2 rounded-md border-gray-500"
+                placeholder="Contraseña"
+                type="password"
+              />
+            </div>
+            <div className="flex flex-col w-32">
+              <label htmlFor="age">Edad</label>
+              <input
+                id="age"
+                className="border p-2 rounded-md border-gray-500"
+                placeholder="Edad"
+                type="number"
+                value={user.age}
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, age: e.target.value }))
+                }
+              />
+            </div>
+          </div>
+          <div className="mt-5">
+            <div className="flex flex-col w-80">
+              <label htmlFor="description">Descripción</label>
+              <textarea
+                id="description"
+                className="border p-2 rounded-md border-gray-500"
+                placeholder="Descripcion"
+                rows={5}
+                value={user.description}
+                onChange={(e) =>
+                  setUser((prev) => ({ ...prev, description: e.target.value }))
+                }
+              />
+            </div>
+          </div>
+          <div className="mt-5">
+            <button className="bg-blue-950 p-2 pl-5 pr-5 rounded-lg text-white hover:bg-blue-900">
+              Guardar Cambios
+            </button>
+          </div>
+        </div>
+        <div className="ml-20 flex flex-col align-middle content-center">
+          <img
+            alt="imagen usuario"
+            src="/images/default-image.jpg"
+            width={240}
+            style={{ height: "220px", maxHeight: "400px", borderRadius: "50%" }}
+          />
+          <div className="mt-8">
+            <button
+              style={{ borderWidth: "3px" }}
+              className="border border-blue-950 p-2 pl-16 ml-2 pr-16 rounded-lg hover:bg-blue-950 hover:text-white"
+            >
+              Eliminar Foto
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default AccountSettings;
