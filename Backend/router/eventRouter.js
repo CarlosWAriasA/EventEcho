32563d18b5e0801765080fd27dc5925c01884e6d
Carlos Arias <carlos.ariasalmanzar@gmail.com>
2024-03-10 22:28:29 -0400

    Agregando ruta de listar eventos de usuarios

--- Backend/router/eventRouter.js
+++ Backend/router/eventRouter.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const eventController = require('../controllers/eventController');
-const { authenticateToken } = require('../middleware/authMiddleware');
+const eventController = require("../controllers/eventController");
+const { authenticateToken } = require("../middleware/authMiddleware");
 
-router.get('/:eventId', authenticateToken, eventController.getEventById);
+router.get("/", authenticateToken, eventController.getAllEvents);
+router.get("/user", authenticateToken, eventController.getAllEventsByUserId);
+router.get("/:eventId", authenticateToken, eventController.getEventById);
 // Define la ruta GET para obtener todos los eventos
-router.get('/', authenticateToken, eventController.getAllEvents);
-
 
 // Rutas para Organizadores (Eventos)
-router.post('/', authenticateToken, eventController.createEvent);
-router.put('/:eventId', authenticateToken, eventController.updateEvent);
-router.delete('/:eventId', authenticateToken, eventController.deleteEvent);
+router.post("/", authenticateToken, eventController.createEvent);
+router.put("/:eventId", authenticateToken, eventController.updateEvent);
+router.delete("/:eventId", authenticateToken, eventController.deleteEvent);
 
 module.exports = router;
