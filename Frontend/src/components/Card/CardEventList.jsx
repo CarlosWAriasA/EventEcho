7a43aaa60c0708c50a9df214581325beb0d9a5cb
Elian Báez <107364306+Elian-Developer@users.noreply.github.com>
2024-03-14 22:43:04 -0400

    Cambios en la UI de mis eventos
    Se realizaron algunos cambios en la seccion de mis eventos.

--- Frontend/src/components/Card/CardEventList.jsx
+++ Frontend/src/components/Card/CardEventList.jsx
@@ -1,24 +1,41 @@
 import { NavLink } from 'react-router-dom';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const CardEventList  = ({ title, content, imageUrl, time, place }) => {
+const CardEventList = ({ title, content, imageUrl, time, place }) => {
   // Default image URL
   const defaultImageUrl = '/images/default-image.jpg';
 
   return (
     <NavLink to={'event-detail'}>
-      <div className='flex m-2 p-4 bg-white rounded-lg shadow-md hover:cursor-pointer border border-black-500 p-4'>
+      <div className='flex m-2 p-4 bg-white  shadow-md hover:cursor-pointer border-2 border-gray-400 rounded-3xl p-4'>
         <img
           src={imageUrl || defaultImageUrl}
           alt='Card'
-          className='text-black-500 w-40 h-44 object-cover mb-4 rounded-md'
+          className='text-black-500 w-60 h-44 object-cover mb-4 rounded-3xl'
         />
         <div className='flex flex-col ml-6'>
-            <h2 className='text-black-500 text-xl font-semibold mb-2'>{title}</h2>
-            <p>{content}</p>
-            <div className='flex mt-10'>
-                <h3 className='mr-40'>{time}</h3> 
-                <h3>{place}</h3>
+          <div className='flex'>
+            <h2 className='text-black-500 text-xl font-semibold mb-2'>
+              {title}
+            </h2>
+          </div>
+          <p>{content}</p>
+          <div className='flex'>
+            <div className='flex mt-20'>
+              <AccessTimeIcon sx={{ color: 'yellow', fontSize: '20px' }} />
+              <h3 className='mr-35 ml-2 w-60'>{time}</h3>
             </div>
+            <div className='flex mt-20'>
+              <LocationOnOutlinedIcon
+                sx={{ color: 'yellow', fontSize: '20px' }}
+              />
+              <h3 className='mr-35 ml-2 w-60'>
+                <h3>{place}</h3>
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </NavLink>
--- Frontend/src/views/Events/EventList.jsx
+++ Frontend/src/views/Events/EventList.jsx
@@ -1,35 +1,72 @@
-import { blue } from "@mui/material/colors"
-import { color } from "@mui/system"
+import { blue } from '@mui/material/colors';
+import { color } from '@mui/system';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import Card from '../../components/Card/CardEventList';
+import CardEventList from '../../components/Card/CardEventList';
 
 const EventList = () => {
-    return (
-     <main className="bg-white text-black" style={{backgroundColor: '#9BA4B5'}}>
+  const EventsData = [
+    {
+      id: 1,
+      title: 'Fiesta en la Plaza',
+      content: 'Acompañanos en nuestra fiesta',
+      time: '6:30',
+      place: 'Plaza de la Bandera',
+    },
+    {
+      id: 2,
+      title: 'Aventura deportiva colectiva',
+      content: 'Aconpañanos en esta aventura deportiva',
+      time: '8:30',
+      place: 'Centro Olimpico',
+    },
+    {
+      id: 3,
+      title: 'Conocedores de Historia',
+      content: 'En la misma conoceremos la historia del faro',
+      time: '6:30',
+      place: 'El faro a Colon',
+    },
+    {
+      id: 4,
+      title: 'Aventureros por la ciudad',
+      content: 'Aventura por la Nuñez de Caceres',
+      time: '6:30',
+      place: 'La nuñez de Caceres',
+    },
+  ];
+
+  return (
+    <main
+      className='bg-white text-black'
+      style={{ backgroundColor: '#D8E1EE' }}
+    >
       <div className='flex justify-inline flex-col'>
-        <div
-          className='flex flex-col columns-6 max-w-screen-xl w-3/4 bg-blue-500 p-8 rounded-lg ml-10 mt-10 text-white'
-          style={{ backgroundColor: '#394867' }}
-        >
-          <div className='flex mb-2'>
-            <h1 style={{ fontSize: '22px' }}>
-              Hora de Salir a comer con Chanchito
-            </h1>
-          </div>
-          <div className='flex mb-2'>
-            <h3 className='pr-2'>
-              <LocationOnOutlinedIcon
-                sx={{ color: 'yellow', fontSize: '20px' }}
-              />
-            </h3>
-            <h3>Santo Domingo, Naco</h3>{' '}
-          </div>
-          <div className='flex mb-2'>
-            <h3 className='pr-2'>
-              <AccessTimeIcon sx={{ color: 'yellow', fontSize: '20px' }} />
-            </h3>
-            <h3>07:30 PM</h3>
+        <div className='flex'>
+          <div
+            className='flex flex-col columns-6  w-3/4 bg-blue-500 p-8 rounded-3xl ml-10 mt-10 text-white'
+            style={{ backgroundColor: '#394867' }}
+          >
+            <div className='flex mb-2'>
+              <h1 style={{ fontSize: '22px' }}>
+                Hora de Salir a comer con Chanchito
+              </h1>
+            </div>
+            <div className='flex mb-2'>
+              <h3 className='pr-2'>
+                <LocationOnOutlinedIcon
+                  sx={{ color: 'yellow', fontSize: '20px' }}
+                />
+              </h3>
+              <h3>Santo Domingo, Naco</h3>{' '}
+            </div>
+            <div className='flex mb-2'>
+              <h3 className='pr-2'>
+                <AccessTimeIcon sx={{ color: 'yellow', fontSize: '20px' }} />
+              </h3>
+              <h3>07:30 PM</h3>
+            </div>
           </div>
         </div>
 
@@ -37,30 +74,19 @@ const EventList = () => {
           className='flex flex-col columns-6 max-w-screen-xl w-3/4 bg-blue-500 p-8 ml-10 mt-10'
           style={{ backgroundColor: '#FCFCFC' }}
         >
-          <Card 
-            title={'La danza espacial del chanchito feliz'}
-            content={'ven a ver el famoso baile de Chanchito feliz'}
-            time={'8:00'}
-            place={'Zona Colonial'}
-            
-          />
-          <Card
-            title={'Fiesta Loca con Chanchito'}
-            content={'Acompañanos a comer sushi y calamar para celebrar la'}
-            time={'6:30'}
-            place={'El Malecon'}
-          />
-          <Card
-            title={'Fiesta en la plya con chanchito'}
-            content={'Acompañanos  en la playa para disfutar del baile de chanchito'}
-            time={'10:00'}
-            place={'Plaza de la Bandera'}
-          />
+          {EventsData.map((card) => (
+            <Card
+              key={card.id}
+              title={card.title}
+              content={card.content}
+              place={card.place}
+              time={card.time}
+            ></Card>
+          ))}
         </div>
       </div>
-     </main>
-    );
-}
+    </main>
+  );
+};
 
-export default EventList
-
+export default EventList;
