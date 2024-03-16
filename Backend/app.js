59bdda240c7339d7b923d41286161ba83cd0a805
JosueNavarrop <josuenavarrop2003janp@gmail.com>
2024-03-16 17:22:10 -0400

    User Crud added

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,7 @@ const userRouter = require("./router/userRouter");
 const userEventsRouter = require("./router/userEventsRouter");
 const sequelize = require("./connection/connection");
 const defineAssociations = require("./connection/associations");
+const userRoutes = require('./router/userRouter');
 
 dotenv.config();
 
@@ -28,6 +29,19 @@ app.use("/api/registro-evento/", userRouter);
 app.use("/api/profile/", profileRouter);
 
 app.use("/api/", userEventsRouter);
+
+//Ruta de los usuarios
+app.use('/api/usuarios/', userRoutes);
+
+
+//Solo para Probar que las Rutas no estan Mal
+app.use((req, res, next) => {
+  res.status(404).json({
+      ok: false,
+      msg: 'Ruta no encontrada'
+  });
+});
+
 
 const PORT = process.env.PORT || 5000;
 
--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -43,4 +43,97 @@ const registrarUsuario = async (req, res) => {
     }
 };
 
-module.exports = { registrarUsuario };
+
+//Obtener usuario por ID
+const obtenerUsuario = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // Buscar el usuario por su ID
+        const usuario = await Usuario.findByPk(id);
+
+        if (!usuario) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El usuario no fue encontrado'
+            });
+        }
+
+        // Devolver el usuario encontrado
+        return res.status(200).json({
+            ok: true,
+            usuario
+        });
+    } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hubo un error al obtener el usuario',
+            error
+        });
+    }
+};
+
+//EDITAR USUARIOS
+const editarUsuario = async (req, res) => {
+    const { id } = req.params;
+    const { name, lastName, username, email, password,tipo_usuario } = req.body;
+
+    try {
+        // Verificar si el usuario existe en la base de datos
+        const usuarioExistente = await Usuario.findByPk(id);
+
+        if (!usuarioExistente) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'El usuario no fue encontrado'
+            });
+        }
+
+        // Actualizar los campos del usuario
+        await usuarioExistente.update({
+            name,
+            lastName,
+            username,
+            email,
+            password,
+            tipo_usuario
+        });
+
+        console.log('Usuario actualizado');
+        return res.status(200).json({
+            ok: true,
+            msg: 'El usuario ha sido actualizado con éxito'
+        });
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hubo un error al actualizar el usuario',
+            error
+        });
+    }
+};
+
+//OBTENER TODOS LOS USUARIOS
+// const obtenerTodosUsuarios = async (req, res) => {
+//     try {
+//         // Buscar todos los usuarios en la base de datos
+//         const usuarios = await Usuario.findAll();
+
+//         // Devolver los usuarios encontrados
+//         return res.status(200).json({
+//             ok: true,
+//             usuarios
+//         });
+//     } catch (error) {
+//         console.error("Error al obtener los usuarios:", error);
+//         res.status(500).json({
+//             ok: false,
+//             msg: 'Hubo un error al obtener los usuarios',
+//             error
+//         });
+//     }
+// };
+
+module.exports = { registrarUsuario, editarUsuario, obtenerUsuario, obtenerTodosUsuarios };
