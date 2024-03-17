6012348231a0462e5432fa516eb81dcd8efd9615
FrankR27 <frankdejesusramosguzman9@gmail.com>
2024-03-16 22:34:49 -0400

    Middleware para manejar rutas inexistentes y devolver un error 404

--- Backend/app.js
+++ Backend/app.js
@@ -35,6 +35,12 @@ app.use('/api/', userRoutes);
 // para servir imagenes estaticas
 app.use('/api/uploads/', express.static(path.join(__dirname, 'uploads')));
 
+// Middleware para manejar rutas inexistentes y devolver un error 404
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Ruta no encontrada" });
+});
+
+
 const PORT = process.env.PORT || 5000;
 
 defineAssociations();
